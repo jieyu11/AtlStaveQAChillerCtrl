9a8206cadaf8f0bb2c3f9427d97469ea9cc71264
jie.yu@cern.ch <jie.yu@cern.ch>
2017-07-20 11:21:25 -0500

    Roy's doc for intro

---
+++ ChillerCtrl.py
@@ -0,0 +1,67 @@
+"""
+	Program ChillerCtrl.py
+	
+Description: ------------------------------------------------------------------
+	A Python script to control the coolant chiller equipment.  The chilling
+equipment is composed of: FTS Systems RC211B0 recirculating cooler; a 
+Lenze ESV751N02YXC NEMA 4x inverter drive; a 3/4 HP, 1800RPM, 60Hz, 3-phase,
+PVN56T17V5338B B motor; and a Liquiflo H5FSPP4B002606US-8(-60) booster pump.
+
+This file contains the main body of code.  
+
+History: ----------------------------------------------------------------------
+	V1.0 - Jul-2017  First public release.
+	
+Environment: ------------------------------------------------------------------
+	This program is written in Python 3.6.  Python can be freely downloaded from 
+http://www.python.org/.  This program has been tested on PCs running Windows 10.
+
+Author List: -------------------------------------------------------------------
+	R. McKay  Iowa State University, USA  mckay@iastate.edu
+	J. Yu  Iowa State University,  USA  jieyu@iastate.edu
+	W. Heidorn  Iowa State University,  USA  wheidorn@iastate.edu
+	
+Notes: -------------------------------------------------------------------------
+
+Dictionary of abbreviations: ---------------------------------------------------
+
+"""
+
+# Import section ---------------------------------------------------------------
+
+# Global data section ----------------------------------------------------------
+
+# Module data section ----------------------------------------------------------
+
+# Local data section -----------------------------------------------------------
+
+# Main code section ------------------------------------------------------------
+
+
+# Upon startup print on computer screen this code version.
+#
+# Create a log text file for historical recording of actions and time stamp with
+# temperture data from all temperature sensors. (humidity?) The log file will 
+# have a date and code version header.  Notify user of success or read issues.
+# 
+# Open and read the configuration file.  This file contains all the settings for
+# the chiller unit, booster pump, and RS-232 & USB:RS-485 serial interfaces.
+# Notify user & log succes or issues.
+#
+# If no issues reading configuration file, 1st set the USB-serial ports.  Send a 
+# status command to the equipment.  If no issues, set all devices to defined 
+# configuration. Notify user & log success or issues setting devices.
+#
+# Open and read the command file for the chiller and booster pump.  Load the
+# commands into a dictionary construct.  Notify user & log success or issues. 
+# If issues reading the command file exit program.
+# Internal to this program are specific commands: Loop, EndLoop, wait, & exit. 
+# 
+# In a infinite loop with shutdown hook, prompt user for a command and execute
+# command. Log commands given.  At set frequency log the time, temperature(s),
+# and humidity.
+#
+# Upon exit command, check chiller has been set to room temperature.  If not,
+# do so.  Inform user of action and log status.  If chiller at room temperature
+# log end of run, close log file and exit.
+#
--- README.md
+++ README.md
@@ -1,3 +1,34 @@
-# code to deal with chiller and boost pump control
+#							                  ATLAS Stave Thermo Quality Assurance Testing
 
-# Will's Test
+Introduction
+
+	The program ChillerCtrl.py purpose is to control the coolant fluid temperature and flow through the ATLAS stave.  
+
+Requirements
+	
+	*Software
+	Any OS that supports Python.  Python 3.6.2 or greater interpreter.  200MB for Python and ?KB for ChillerCtrl.py. 
+	
+	*Hardware
+	A computer with at least one RS-232 serial port and 4 USB ports.  One USB port is expected to have a USB to RS-485 adapter.
+	The cooling system is composed of: FTS System RC211B0 recirculating cooler; Lenze ESV751N02YXC EMA 4x inverter drive;
+	pump motor PVN56T17V5338B; and Liquiflo H5FSPP4B002606US-8(-60) booster pump.  The coolant used is 3M Novec 7100. 
+	One of each: Omega HH147U temperature logger, Omega HH314A humidity logger, and Flir A655sc IR camera.
+
+Installation
+
+	Follow Python installer instructions for installing Python.  Place the ChillerCtrl.py in any desired directory.
+	
+Usage
+
+	?
+	
+History
+
+	?-Aug-2017 version 1.0 released.
+
+Authors
+
+	William Heidorn, Iowa State University,  USA  wheidorn@iastate.edu
+	Jie Yu, Iowa State University,  USA  jieyu@iastate.edu
+	Roy Mckay, Iowa State University, USA  mckay@iastate.edu
